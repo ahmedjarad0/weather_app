7c9f32a9792cdff83a940e6cf7f99e4ff1122152
ahmedjarad0 <0597181403anj@gmail.com>
2023-08-13 20:00:39 +0300

    second commit

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -1,3 +1,4 @@
+import 'package:country_state_city_pro/country_state_city_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:weather_app/services/networking.dart';
 import 'package:weather_app/services/weather_model.dart';
@@ -10,9 +11,25 @@ class CityScreen extends StatefulWidget {
 }
 
 class _CityScreenState extends State<CityScreen> {
+ late TextEditingController country ;
+ late TextEditingController state ;
+ late TextEditingController cityC ;
   WeatherModel weatherModel = WeatherModel();
   String? city;
-
+@override
+  void initState() {
+    super.initState();
+    country =TextEditingController();
+    state =TextEditingController();
+    cityC =TextEditingController();
+  }
+  @override
+  void dispose() {
+  country.dispose();
+  state.dispose();
+  cityC.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,25 +60,17 @@ class _CityScreenState extends State<CityScreen> {
                     const SizedBox(
                       height: 40,
                     ),
-                    TextField(
-                      onChanged: (value) {
-                        setState(() {
-                          city = value;
-                        });
-                      },
-                      decoration: InputDecoration(
-                          fillColor: Colors.white,
-                          filled: true,
-                          focusColor: Colors.grey,
-                          icon: const Icon(
-                            Icons.location_city,
-                            color: Colors.blue,
-                          ),
-                          hintText: 'Enter City Name',
-                          border: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.white),
-                            borderRadius: BorderRadius.circular(10),
-                          )),
+
+                    CountryStateCityPicker(
+                        country: country,
+                        state: state,
+                        city: cityC,
+                        dialogColor: Colors.grey.shade200,
+                        textFieldDecoration: InputDecoration(
+                            fillColor: Colors.blueGrey.shade100,
+                            filled: true,
+                            suffixIcon: const Icon(Icons.arrow_downward_rounded),
+                            border: const OutlineInputBorder(borderSide: BorderSide.none))
                     ),
                     const SizedBox(
                       height: 20,
@@ -70,7 +79,7 @@ class _CityScreenState extends State<CityScreen> {
                       child: TextButton(
                           onPressed: () async {
                             Map<String, dynamic> weatherData =
-                                await NetworkHelper().getCityData(city!);
+                                await NetworkHelper().getCityData(country.text);
                             Navigator.pop(context, weatherData);
                           },
                           child: const Text(
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:weather_app/screens/city_screen.dart';
 import 'package:weather_app/services/weather_model.dart';
